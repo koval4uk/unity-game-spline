492b5d56361f0f793cdf92779deb58ffd408e78c
walker2pac <butchersgameskg@gmail.com>
2020-10-21 16:24:37 +0600

    changed railways

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     private PlayerEvents events;
     private SplineFollower follower;
     private float movementSpeed = 2.0f;
+    private float limitMovementSpeed = 28.0f;
+    private float increaseSpeedStep = 0.45f;
 
     private void Awake()
     {
@@ -35,5 +37,22 @@ public class PlayerMovement : MonoBehaviour
     private void StartMove()
     {
         follower.follow = true;
+        StartCoroutine(IncreaseSpeed());
+    }
+
+    private IEnumerator IncreaseSpeed()
+    {
+        Debug.Log("Increase Speed Start!");
+        while (movementSpeed < limitMovementSpeed)
+        {
+            yield return new WaitForSeconds(0.1f);
+            UpdateSpeed();
+        }
+    }
+
+    private void UpdateSpeed()
+    {
+        movementSpeed += increaseSpeedStep;
+        follower.followSpeed = movementSpeed;
     }
 }
