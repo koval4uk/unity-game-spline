b135f3456c0754400964621269c24d632785d653
walker2pac <butchersgameskg@gmail.com>
2020-10-23 23:42:19 +0600

    added ui to enemy

---
+++ Assets/Scripts/Enemy/EnemyUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUI : MonoBehaviour
+{
+    [SerializeField] private TextMesh textMesh;
+
+    private StickmanEvents stickmanEvents;
+
+    private void Awake()
+    {
+        stickmanEvents = GetComponent<StickmanEvents>();
+    }
+
+    private void ChangePlace()
+    {
+        // Change Placement Text
+    }
+    
+}
