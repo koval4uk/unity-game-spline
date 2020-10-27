ea50e42d122712fc0be327f406e99ff669c582c0
walker2pac <butchersgameskg@gmail.com>
2020-10-27 19:31:44 +0600

    added wall explosion animation

--- Assets/Scripts/Obstacles/BrickExplosion.cs
+++ Assets/Scripts/Obstacles/BrickExplosion.cs
@@ -1,8 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BrickExplosion : MonoBehaviour
 {
+    private Rigidbody rigidbody;
+    private float explodePower = 10.0f;
+    private float explodeRadius = 5.0f;
+    private float explodeUpForce = 2.0f;
 
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Explode(Vector3 explodePosition)
+    {
+        rigidbody.useGravity = true;
+        rigidbody.isKinematic = false;
+        rigidbody.AddExplosionForce(explodePower, explodePosition, explodeRadius, explodeUpForce, ForceMode.Impulse);
+    }
 }
--- Assets/Scripts/Obstacles/Wall.cs
+++ Assets/Scripts/Obstacles/Wall.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Wall : MonoBehaviour, IObstacle
 {
+    private BrickExplosion[] allBricks;
+    
+    private void Awake()
+    {
+        allBricks = GetComponentsInChildren<BrickExplosion>();
+    }
+
     public void Initiate(StickmanEvents stickmanEvents)
     {
         stickmanEvents.OnChangeSpeed(2.0f);
-        DestroyAnimation();
+        StartCoroutine(DestroyAnimation(stickmanEvents.transform.position));
     }
 
-    private void DestroyAnimation()
+    private IEnumerator DestroyAnimation(Vector3 explodePosition)
     {
-        // Beautiful animation
+        Debug.Log("Destroy Animation");
+        foreach (var brick in allBricks)
+        {
+            brick.Explode(explodePosition);
+        }
+        yield return new WaitForSeconds(4.0f);
         gameObject.SetActive(false);
     }
 }
