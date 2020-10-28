b4eb010eff0ff0f1f84d1324a2251083c0b73d3b
saw0013 <61006293+saw0013@users.noreply.github.com>
2020-10-28 15:30:14 +0400

    Add files via upload
    Test

---
+++ Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public string name;
+    
+    void Start()
+    {
+      name = "HelloWorld"; 
+    }
+
+ 
+}
