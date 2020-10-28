3bff5b66e0c6f1308a1c8c28c1cb027e5f5d0c99
Artur Kovalchuk <artur.kovalchuk@worldapp.com>
2020-10-28 13:25:56 +0200

    Fix PositionSystem

--- Assets/Scripts/PositionSystem.cs
+++ Assets/Scripts/PositionSystem.cs
@@ -1,30 +1,43 @@
-﻿using Dreamteck.Splines;
+﻿using System.Linq;
+using System.Numerics;
+using Dreamteck.Splines;
 using TMPro;
 using UnityEngine;
 
 public class PositionSystem : MonoBehaviour
 {
-    private const string FirstPlace = "1";
-    private const string SecondPlace = "2";
+
+    private SplineFollower[] _splineFollowers;
+
     
-    [SerializeField] private SplineFollower player;
-    [SerializeField] private TextMeshPro playerPositionBoard;
-
-    [SerializeField] private SplineFollower enemy;
-    [SerializeField] private TextMeshPro enemyPositionBoard;
-
-
+    private void Start()
+    {
+        _splineFollowers = FindObjectsOfType<SplineFollower>();
+        //  FindObjectsOfType<SplineFollower>()[1].gameObject.GetComponentInChildren<TextMeshPro>()
+    }
+    
     private void Update()
     {
-        /*if (player.result.percent > enemy.result.percent)
-        {
-            playerPositionBoard.SetText(FirstPlace);
-            enemyPositionBoard.SetText(SecondPlace);
-        }
-        else
-        {
-            playerPositionBoard.SetText(SecondPlace);
-            enemyPositionBoard.SetText(FirstPlace);
-        }*/
+        BigInteger numberInRace = 1;
+        var sortedSplineFollowers = sortPlayersByProgress();
+        updatePositionForPlayer(sortedSplineFollowers, numberInRace);
+    }
+
+    private void updatePositionForPlayer(SplineFollower[] sortedSplineFollowers, BigInteger numberInRace)
+    {
+        sortedSplineFollowers
+            .Select(player =>
+            {
+                TextMeshPro textMeshPro = player.gameObject.GetComponentInChildren<TextMeshPro>();
+                textMeshPro.SetText(numberInRace++.ToString());
+                return true;
+            })
+            .ToArray();
+    }
+
+    private SplineFollower[] sortPlayersByProgress()
+    {
+        return _splineFollowers.OrderBy(splineFollower => -splineFollower.result.percent)
+                .ToArray();
     }
 }
