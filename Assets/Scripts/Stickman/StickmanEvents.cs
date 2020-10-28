3e3350acac9ed74015a52bcdffe2fddbb00f85f6
walker2pac <butchersgameskg@gmail.com>
2020-10-28 18:34:20 +0600

    added logging for movement bug

--- Assets/Scripts/Stickman/StickmanEvents.cs
+++ Assets/Scripts/Stickman/StickmanEvents.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class StickmanEvents : MonoBehaviour
 {
-    public Action OnMove = delegate { Debug.Log("OnMove on Triggered!" ); };
+    public Action OnMove = delegate { Debug.Log("OnMove Triggered!" ); };
     public Action<float> OnChangeSpeed = delegate { Debug.Log("OnChangeSpeed Triggered!"); };
     public Action<float> OnMultiplySpeed = delegate { Debug.Log("OnMultiplySpeed Triggered!"); };
     public Action OnSetInitialSpeed = delegate { Debug.Log("OnSetInitialSpeed Triggered!"); };
