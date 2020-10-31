44af55a79c8b4376fc752e6a88d2ae4e49959b6b
walker2pac <butchersgameskg@gmail.com>
2020-10-31 15:30:01 +0600

    added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using Object = System.Object;
+
+public class CameraController : MonoBehaviour
+{
+    private CinemachineVirtualCamera camera;
+
+    private void Awake()
+    {
+        camera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void OnEnable()
+    {
+        Observer.Instance.OnWinLevel += StopFollow;
+    }
+
+    private void StopFollow()
+    {
+        camera.Follow = null;
+    }
+}
