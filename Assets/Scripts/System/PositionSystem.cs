86274e42e2dfb97c11a7e1f1ff30fec666d02ed5
Artur Kovalchuk <artur.kovalchuk@worldapp.com>
2020-11-02 22:26:54 +0200

    Add GetPlayerPosition() method

--- Assets/Scripts/System/PositionSystem.cs
+++ Assets/Scripts/System/PositionSystem.cs
@@ -1,48 +1,33 @@
 ﻿using System;
 using System.Linq;
 using Dreamteck.Splines;
-using TMPro;
 using UnityEngine;
 
 public class PositionSystem : MonoBehaviour
 {
-
-    private SplineProjector[] _splineProjectors;
+    
+    private SplineProjector[] splineProjectors;
     
     private void Start()
     {
-        _splineProjectors = FindObjectsOfType<SplineProjector>()
+        splineProjectors = FindObjectsOfType<SplineProjector>()
             .Where(c => c.CompareTag(GameConstants.TagMainProjector))
+            .ToList()
+            .ConvertAll(splineProjector => {
+                    splineProjector.spline = RailwaysManager.Instance.MainRailway;
+                    return splineProjector;
+            })
             .ToArray();
     }
     
-    private void Update()
+    public int GetPlayerPosition()
     {
-        var sortedSplineProjectors = SortPlayersByProgress();
-        UpdatePositionForPlayer(sortedSplineProjectors);
-    }
-
-    private SplineProjector[] SortPlayersByProgress()
-    {
-        Array.ForEach(_splineProjectors, splineProjector =>
-            {
-                splineProjector.spline = RailwaysManager.Instance.MainRailway;
-            });
-        
-        return _splineProjectors
+        int indexPlayerAmongAllProjectors = splineProjectors
             .OrderBy(player => -player.result.percent)
-            .ToArray();
+            .ToList()
+            .FindIndex(projector => projector.name == "PlayerPosition");
+        
+        return ++indexPlayerAmongAllProjectors;
     }
 
-    private static void UpdatePositionForPlayer(SplineProjector[] sortedSplineProjectors)
-    {
-        int initNumberInRace = 1;
-        
-        Array.ForEach(sortedSplineProjectors, splineProjector =>
-            {
-                var textMeshPro = splineProjector.gameObject.GetComponentInChildren<TextMeshPro>();
-                textMeshPro.SetText(initNumberInRace++.ToString());
-            });
-    }
-    
 }
